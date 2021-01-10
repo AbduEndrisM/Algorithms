b3ac3edf31bd2e31f3d98b13cf8316903755a063
Abdu <beazsasm@gmail.com>
2021-01-09 21:57:49 -0600

    1119. Remove Vowels from a String - v2

--- Java/Amazon/RemoveVowelsFromString.java
+++ Java/Amazon/RemoveVowelsFromString.java
@@ -7,24 +7,50 @@ public class RemoveVowelsFromString {
 
     public static void main(String[] args) {
         String string = "abdu";
-        String result = removeVowelsFromString(string);
+        String result = removeVowelsFromStringIII(string);
 
         System.out.println(result);
     }
 
     private static String removeVowelsFromString(String string) {
 
-        StringBuilder stringBuilder = new StringBuilder();
-        String s = ""; // OPTION TWO
+        String s = "";
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) != 'a' && string.charAt(i) != 'e' && string.charAt(i) != 'i' &&
                     string.charAt(i) != 'o' && string.charAt(i) != 'u') {
-                stringBuilder.append(string.charAt(i));
 
                 s = s + string.charAt(i);
             }
         }
-//        return stringBuilder.toString();
         return s;
+    }
+
+
+    private static String removeVowelsFromStringII(String string) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+
+            if (string.charAt(i) != 'a' && string.charAt(i) != 'e' && string.charAt(i) != 'i' &&
+                    string.charAt(i) != 'o' && string.charAt(i) != 'u') {
+                stringBuilder.append(string.charAt(i));
+
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String removeVowelsFromStringIII(String string) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+                stringBuilder.append(ch);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
