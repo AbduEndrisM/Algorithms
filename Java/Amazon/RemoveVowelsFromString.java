ccb0b150b729fd45ef5f5c114f69776349a54c8b
Abdu <beazsasm@gmail.com>
2021-01-09 22:23:11 -0600

    1119. Remove Vowels from a String - v3

--- Java/Amazon/RemoveVowelsFromString.java
+++ Java/Amazon/RemoveVowelsFromString.java
@@ -7,9 +7,28 @@ public class RemoveVowelsFromString {
 
     public static void main(String[] args) {
         String string = "abdu";
-        String result = removeVowelsFromStringIII(string);
+        System.out.println("String " + removeVowelsFromString(string));
+        System.out.println("StringBuilder " + removeVowelsFromStringII(string));
+        System.out.println("RegEx " + removeVowelsFromStringB(string));
+        System.out.println("CharArray " + removeVowelsFromStringC(string));
 
-        System.out.println(result);
+    }
+
+    private static String removeVowelsFromStringC(String string) {
+        char[] chars = new char[string.length()];
+
+        int index = 0;
+        for (char c :
+                string.toCharArray()) {
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')
+                chars[index++] = c;
+        }
+        return new String(chars, 0, index);
+    }
+
+    private static String removeVowelsFromStringB(String string) {
+
+        return string.replaceAll("[a,e,i,o,u]", "");
     }
 
     private static String removeVowelsFromString(String string) {
@@ -27,21 +46,6 @@ public class RemoveVowelsFromString {
 
 
     private static String removeVowelsFromStringII(String string) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < string.length(); i++) {
-
-            if (string.charAt(i) != 'a' && string.charAt(i) != 'e' && string.charAt(i) != 'i' &&
-                    string.charAt(i) != 'o' && string.charAt(i) != 'u') {
-                stringBuilder.append(string.charAt(i));
-
-            }
-        }
-        return stringBuilder.toString();
-    }
-
-    private static String removeVowelsFromStringIII(String string) {
 
         StringBuilder stringBuilder = new StringBuilder();
 
